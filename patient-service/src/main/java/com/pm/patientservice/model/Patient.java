de03ee0de5225a023a23c3a00f7d5a9a59425c89
Jonathan <jontyrif@gmail.com>
2026-05-19 17:47:58 +0530

    Added Getter and Setter annotations to the Patient entity

--- patient-service/src/main/java/com/pm/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -3,13 +3,16 @@ package com.pm.patientservice.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 public class Patient {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
